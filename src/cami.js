f1ec3fdb43f63c58d24e0c28582de24cc4b5eef8
kennyfrc <fxkennyfrc@gmail.com>
2023-11-29 00:41:05 +0800

    feat: add `svg` and `css`

--- src/cami.js
+++ src/cami.js
@@ -7,7 +7,8 @@
 /**
  * @module cami
  */
-import { html, render } from 'lit-html';
+import { html, render, svg } from 'lit-html';
+import { css } from 'goober';
 import { produce } from "immer"
 import { ReactiveElement } from './reactive-element.js';
 import { ObservableStore, store } from './observables/observable-store.js';
@@ -24,6 +25,8 @@ const { debug, events } = _config;
 /**
  * @exports store - The store object from observable-store.js. This uses local storage by default.
  * @exports html - The html function from 'lit-html'
+ * @exports svg - The svg function from 'lit-html'
+ * @exports css - The css function from 'goober'
  * @exports ReactiveElement - The ReactiveElement class from reactive_element.js
  * @exports ObservableStream - The ObservableStream class from observable-stream.js
  * @exports ObservableElement - The ObservableElement class from observable-element.js
@@ -34,4 +37,4 @@ const { debug, events } = _config;
  * @exports debug - The debug property from _config
  * @exports events - The events property from _config
  */
-export { store, html, ReactiveElement, ObservableStream, ObservableElement, Observable, ObservableState, ObservableStore, http, debug, events };
+export { store, html, svg, css, ReactiveElement, ObservableStream, ObservableElement, Observable, ObservableState, ObservableStore, http, debug, events };
